9c7908507b2ad19b8fe8e2156b8f82e69b650151
hulyakizrakyigit <hhulyakizrakk@gmail.com>
2024-09-24 16:38:00 +0300

    Fix file replacement and update file handling

--- CPP_01/ex04/Replace.cpp
+++ CPP_01/ex04/Replace.cpp
@@ -2,14 +2,14 @@
 
 void replacInFile(std::string& filename, std::string& s1, std::string& s2)
 {
-	std::ifstream infile(filename); // open file
+	std::ifstream infile(filename);
 	if (!infile.is_open())
 	{
 		std::cout << "Error: could not open file" << std::endl;
 		return;
 	}
 
-	std::ofstream outfile(filename + ".replace"); // create file
+	std::ofstream outfile(filename + ".replace");
 	if (!outfile.is_open())
 	{
 		std::cout << "Error: could not create file" << std::endl;
@@ -17,18 +17,18 @@ void replacInFile(std::string& filename, std::string& s1, std::string& s2)
 	}
 
 	std::string line;
-	while (std::getline(infile, line)) // read line by line
+	while (std::getline(infile, line))
 	{
 		size_t pos = 0;
 		std::string  new_line;
-		while ((pos = line.find(s1, pos)) != std::string::npos) // find s1 in line
+		while ((pos = line.find(s1, pos)) != std::string::npos)
 		{
-			new_line += line.substr(0, pos); // add line before s1
-			new_line += s2; // add s2
-			line = line.substr(pos + s1.length()); // remove s1 from line
+			new_line += line.substr(0, pos);
+			new_line += s2;
+			line = line.substr(pos + s1.length());
 		}
-		new_line += line; // add the rest of the line
-		outfile << new_line << std::endl; // write line to file
+		new_line += line;
+		outfile << new_line << std::endl;
 	}
 	infile.close();
 	outfile.close();
