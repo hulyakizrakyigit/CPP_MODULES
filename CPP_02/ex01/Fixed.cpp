203610e81e8f5b9e15645f253cab55bfcba37010
hulyakizrakyigit <hhulyakizrakk@gmail.com>
2024-09-25 18:11:54 +0300

    Add Fixed class implementation(cpp02_ex00_ex01)

---
+++ CPP_02/ex01/Fixed.cpp
@@ -0,0 +1,56 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _val(0){
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Default destructor called" << std::endl;
+}
+
+Fixed::Fixed(const int val) {
+	std::cout << "Int constructor called" << std::endl;
+	_val = val << _fractionalBits;
+}
+
+Fixed::Fixed(const float val) {
+	std::cout << "Float constructor called" << std::endl;
+	_val = roundf(val * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed &other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->_val = other.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_val;
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_val = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return (float)this->_val / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const {
+	return this->_val >> _fractionalBits;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed) {
+	out << fixed.toFloat();
+	return out;
+}
+
+
---
+++ CPP_02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int _val;
+		static const int _fractionalBits = 8;
+
+	public:
+	Fixed();
+	Fixed(const int val);
+	Fixed(const float val);
+	~Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+
+#endif
