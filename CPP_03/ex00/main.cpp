6bfbfdb8ee1636b74a32304955df4f09f34be902
hulyakizrakyigit <131719109+hulyakizrakyigit@users.noreply.github.com>
2024-10-14 23:53:23 +0300

    ClapTrap class implementation and cpp03-ex00 done

---
+++ CPP_03/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "ClapTrap.hpp"
+
+int main() {
+    ClapTrap c1;
+    ClapTrap c2("Hunter");
+
+    c1.attack("enemy1");
+    c1.takeDamage(5);
+    c1.beRepaired(3);
+    c1.takeDamage(15);
+    c1.beRepaired(10);
+    c1.attack("enemy2");
+    for (int i = 0; i < 11; i++) {
+    c2.attack("enemy3");
+    }
+    c2.beRepaired(5);
+    return 0;
+}
