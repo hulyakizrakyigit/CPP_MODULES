2275b03039ea36e2e6b51182b0cffa8bcb3db9f1
hulyakizrakyigit <hhulyakizrakk@gmail.com>
2024-12-08 20:31:56 +0300

    Add Animal, Cat, and Dog classes

---
+++ CPP_04/ex00/inc/Animal.hpp
@@ -0,0 +1,11 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal {
+	protected:
+		std::string type;
+};
+
+#endif
---
+++ CPP_04/ex00/inc/Dog.hpp
@@ -0,0 +1,10 @@
+#ifndef DOG_HPPP
+#define DOG_HPPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal {
+
+};
+
+#endif
