6aff5bd90197bb2fef6d100ea1a459a4189079bd
hulyakizrakyigit <hhulyakizrakk@gmail.com>
2025-01-13 16:17:20 +0300

    Add support for deque in EasyFind example and update valueToFind in main

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "EasyFind.hpp"
 #include <vector>
 #include <list>
+#include <deque>
+#include <set>
 
 int main() {
     std::vector<int> v;
@@ -31,7 +33,7 @@ int main() {
     lst.push_back(4);
     lst.push_back(5);
 
-    int valueToFind = 5;
+    int valueToFind = 9;
     try {
         std::list<int>::iterator it = easyFind(lst, valueToFind);
         std::cout << "Value found: " << *it << std::endl;
@@ -39,7 +41,20 @@ int main() {
         std::cout << e.what() << std::endl;
     }
 
+    std::deque<int> dq;
+    dq.push_back(100);
+    dq.push_back(200);
+    dq.push_back(300);
+    dq.push_back(400);
 
+    valueToFind = 200;
+
+    try {
+        std::deque<int>::iterator it = easyFind(dq, valueToFind);
+        std::cout << "Value found: " << *it << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
